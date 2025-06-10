901502853e5cd3a4efe6af8a7c821d4aef1c6616
Nicolas Danquigny <nicolas.danquigny@outlook.fr>
2025-06-10 15:08:25 +0200

    correction routes avec appel front

--- functions/src/routes/user/getUserByEmail.ts
+++ functions/src/routes/user/getUserByEmail.ts
@@ -1,20 +1,16 @@
-import { Router } from "express";
+import { Request, Response } from "express";
 import { pool } from "../../utils/db";
 
-const router = Router();
-
-router.get("/", async (req, res) => {
+export async function getUserByEmail(req: Request, res: Response) {
   const email = req.query.email;
   if (!email) return res.status(400).json({ message: "Email manquant" });
 
   try {
-    const [rows] = await pool.query("SELECT * FROM User WHERE email_user = ?", [email]) as [import('mysql2/promise').RowDataPacket[], any];
-    if (rows.length === 0) return res.status(404).json({ message: "Utilisateur non trouvé" });
-    return res.status(200).json(rows[0]);
+    const [rows] = await pool.query("SELECT * FROM User WHERE email_user = ?", [email]);
+    if ((rows as any[]).length === 0) return res.status(404).json({ message: "Utilisateur non trouvé" });
+    return res.status(200).json(rows);
   } catch (err) {
     console.error("Erreur MySQL :", err);
     return res.status(500).json({ message: "Erreur serveur" });
   }
-});
-
-export default router;
+}
