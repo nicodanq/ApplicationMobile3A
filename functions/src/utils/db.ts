df0f11432d67992c2a374b38fa80fa52df4a33d9
Nicolas Danquigny <nicolas.danquigny@outlook.fr>
2025-06-10 21:50:50 +0200

    ajout var config

--- functions/src/utils/db.ts
+++ functions/src/utils/db.ts
@@ -1,10 +1,11 @@
 // src/utils/db.ts
 import * as dotenv from 'dotenv';
 import * as mysql from 'mysql2/promise';
+
 dotenv.config();
 
 export const pool = mysql.createPool({
-  host: "10.1.120.58",
+  host: process.env.IP_WIFI,
   user: "firebaseuser",
   password: "firebase123",
   database: "application3A",
